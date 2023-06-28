b2d27f1a08a26abde190e2656c5b8e11d24e7f02
delarocamcas <mdelaroca@calacademy.org>
2023-06-27 17:26:42 -0700

    added dummy data, and initial files, added ip updator script

---
+++ specify7_ipup.py
@@ -0,0 +1,67 @@
+"""this document is for seeking out files containing absolute ip address in the specify7 files, and
+   replace them with the current absolute ip address"""
+# importing packages
+import socket
+import os
+import re
+
+def ip_getter():
+    """ip_getter:
+       gets a copy of the host machine's absolute ip address
+       returns:
+            ip_ad: a copy of the host machine's absolute ip address"""
+    hostname = socket.gethostname()
+    ip_ad = socket.gethostbyname(hostname)
+    print(ip_ad)
+    return ip_ad
+
+
+def set_direct():
+    """get_direct: changes directory to
+       current file location directory
+       returns:
+            none
+            """
+    # get current directory
+    current_directory = os.getcwd()
+    # set current directory
+    os.chdir(current_directory)
+
+
+def ip_replace(filename: str):
+    """ip_replace: opens a target file path, and replaces,
+                   all references to ip addresses
+                   to the current ip address
+    """
+    with open(filename, 'r') as file:
+        # Read the contents of the file
+        content = file.read()
+    ip_ad = ip_getter()
+    # Replace the string
+    new_content = re.sub(r'\b10.1.12.\w*\b', ip_ad, content)
+
+    # Open the file in write mode
+    with open(filename, 'w') as file:
+        # Write the modified content back to the file
+        file.write(new_content)
+
+
+
+#def ip_container_up():
+
+
+def master_run():
+    """runs all functions in this file sequentially"""
+    set_direct()
+
+    ip_replace('../specify7/docker-compose.yml')
+
+    ip_replace('settings.py')
+
+    ip_replace('image_client/server_host_settings.py')
+
+
+# running master function
+master_run()
+
+print(socket.gethostname())
