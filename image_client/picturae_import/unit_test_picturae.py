2d3fadc85d905c09139cd39e59000d8f893ff030
delarocamcas <mdelaroca@calacademy.org>
2023-06-29 16:56:35 -0700

    added setUp and TearDown functions to unittests, split into two classes,  reduced size of file_present function, added csv reader function

--- image_client/picturae_import/unit_test_picturae.py
+++ image_client/picturae_import/unit_test_picturae.py
@@ -1,43 +1,130 @@
 """This file contains unit tests for picturae_import.py"""
 from picturae_import import *
 import unittest
+import pytest
 import os
-from datetime import date
+import shutil
+from datetime import date, timedelta
 
 
-class TestFilePresent(unittest.TestCase):
+class TestDirectory(unittest.TestCase):
     """TestFilePresent: a series of unit tests on the file_present
        function in picturae_import.py"""
+
     def test_working_directory(self):
-        """test if in correct working folder"""
+        """test if user in correct working folder"""
         expected_relative = "picturae_import"
         current_dir = os.getcwd()
         _, last_directory = os.path.split(current_dir)
         self.assertEqual(expected_relative, last_directory)
 
     def test_directory(self):
-        """test if working folder contains correct subdirectory"""
+        """tests if working folder contains correct subdirectory picturae_csv"""
         dir_pre = os.path.isdir("picturae_csv")
         self.assertTrue(dir_pre)
 
-    def test_files_confirm(self):
-        """test if test folders with input errors produce correct output"""
-        self.assertEqual(file_present(date_override=True, new_date="2023-6-28"), 2)
-
-        with self.assertRaises(ValueError) as cm:
-            file_present(date_override=True, new_date="2023-6-27")
-            self.assertEqual(str(cm.exception), "Specimen csv does not exist")
-
-        with self.assertRaises(ValueError) as cm:
-            file_present(date_override=True, new_date="2023-2-27")
-            self.assertEqual(str(cm.exception), "Folder csv does not exist")
-
-
-    def raise_error(self):
+    def test_missing_folder_raise_error(self):
         """checks if incorrect sub_directory raises error"""
         with self.assertRaises(ValueError) as cm:
             file_present()
         self.assertEqual(str(cm.exception), f"subdirectory for {date.today()} not present")
+
+
+class TestPaths(unittest.TestCase):
+    """tests paths for file_present function in fixture scope"""
+
+    def setUp(self):
+
+        print("setup called!")
+        # setting date
+        old_date = date.today() - timedelta(days=365 * 20)
+
+        today_date = date.today()
+        # create test directories
+
+        date_list = [str(old_date), str(today_date)]
+
+        for date_string in date_list:
+
+            expected_folder_path = 'picturae_csv/' + str(date_string) + '/picturae_folder(' + \
+                                   str(date_string) + ').csv'
+            expected_specimen_path = 'picturae_csv/' + str(date_string) + '/picturae_specimen(' + \
+                                     str(date_string) + ').csv'
+        # making the directories
+            os.makedirs(os.path.dirname(expected_folder_path), exist_ok=True)
+            os.makedirs(os.path.dirname(expected_specimen_path), exist_ok=True)
+
+            open(expected_folder_path, 'a').close()
+            open(expected_specimen_path, 'a').close()
+
+    def test_expected_path_date(self):
+        """makes temporary folders, and csvs with today's date, to test function,
+          when not overriden"""
+        self.assertEqual(file_present(), 2)
+
+    def test_raise_specimen(self):
+        # removing test path specimen
+        os.remove('picturae_csv/' + str(date.today()) + '/picturae_specimen(' +
+                  str(date.today()) + ').csv')
+        with self.assertRaises(ValueError) as cm:
+            file_present()
+        self.assertEqual(str(cm.exception), "Specimen csv does not exist")
+
+    def test_raise_folder(self):
+        # removing test path folder
+        os.remove('picturae_csv/' + str(date.today()) + '/picturae_folder(' +
+                  str(date.today()) + ').csv')
+        with self.assertRaises(ValueError) as cm:
+            file_present()
+        self.assertEqual(str(cm.exception), "Folder csv does not exist")
+
+
+    def test_files_override_both(self):
+        """tests if test folders work for override= True"""
+        twenty_years = date.today() - timedelta(days=365 * 20)
+
+        # writing assert statement
+        self.assertEqual(file_present(date_override=True, new_date=twenty_years), 2)
+
+    def test_raise_specimen_override(self):
+        twenty_years = date.today() - timedelta(days=365 * 20)
+
+        os.remove('picturae_csv/' + str(twenty_years) + '/picturae_specimen(' +
+                  str(twenty_years) + ').csv')
+
+        with self.assertRaises(ValueError) as cm:
+            file_present(date_override=True, new_date=twenty_years)
+        self.assertEqual(str(cm.exception), "Specimen csv does not exist")
+
+    def test_raise_folder_override(self):
+        twenty_years = date.today() - timedelta(days=365 * 20)
+
+        os.remove('picturae_csv/' + str(twenty_years) + '/picturae_folder(' +
+                  str(twenty_years) + ').csv')
+
+        with self.assertRaises(ValueError) as cm:
+            file_present(date_override=True, new_date=twenty_years)
+
+        self.assertEqual(str(cm.exception), "Folder csv does not exist")
+
+
+    def tearDown(self):
+        """removes path for given created folder"""
+
+        print("teardown called!")
+
+        old_date = date.today() - timedelta(days=365 * 20)
+
+        today_date = date.today()
+
+        date_list = [str(old_date), str(today_date)]
+        # create test directories
+
+        for date_string in date_list:
+            expected_folder_path = 'picturae_csv/' + str(date_string) + '/picturae_folder(' + \
+                                   str(date_string) + ').csv'
+
+            shutil.rmtree(os.path.dirname(expected_folder_path))
 
 
 if __name__ == "__main__":
