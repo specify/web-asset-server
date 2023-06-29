06d2aa1c250ea8a7124755c195893eab01430162
delarocamcas <mdelaroca@calacademy.org>
2023-06-29 16:56:35 -0700

    added setUp and TearDown functions to unittests, split into two classes,  reduced size of file_present function, added csv reader function

--- image_client/picturae_import/picturae_import.py
+++ image_client/picturae_import/picturae_import.py
@@ -2,6 +2,8 @@
 import os
 from datetime import date
 
+import pandas as pd
+
 
 # may not need
 def to_current_directory():
@@ -27,57 +29,119 @@ def file_present(date_override=None, new_date=None):
     """
     to_current_directory()
     files_present = 0
+    folder_date = None
+
     if date_override is None:
+        folder_date = date.today()
 
-        dir_sub = os.path.isdir(str("Production/") + str(date.today()))
+    if date_override is True:
+        folder_date = new_date
 
-        if dir_sub is True:
-            folder_path = 'picturae_csv/' + str(date.today()) + '/picturae_folder(' + \
-                            str(date.today()) + ').csv'
+    # print(str("picturae_csv/") + str(folder_date))
 
-            specimen_path = 'picturae_csv/' + str(date.today()) + '/picturae_specimen(' + \
-                            str(date.today()) + ').csv'
+    dir_sub = os.path.isdir(str("picturae_csv/") + str(folder_date))
 
-            if os.path.exists(folder_path):
-                files_present += 1
-            else:
-                raise ValueError("Folder csv does not exist")
+    # print(dir_sub)
 
-            if os.path.exists(specimen_path):
-                print("Specimen csv exists!")
-                files_present += 1
-            else:
-                raise ValueError("Specimen_csv does not exist")
-        else:
-            raise ValueError(f"subdirectory for {date.today()} not present")
-    else:
-        folder_path = 'picturae_csv/' + new_date + '/picturae_folder(' + \
-                      new_date + ').csv'
+    if dir_sub is True:
+        folder_path = 'picturae_csv/' + str(folder_date) + '/picturae_folder(' + \
+                      str(folder_date) + ').csv'
 
-        specimen_path = 'picturae_csv/' + new_date + '/picturae_specimen(' + \
-                        new_date + ').csv'
+        specimen_path = 'picturae_csv/' + str(folder_date) + '/picturae_specimen(' + \
+                        str(folder_date) + ').csv'
 
         if os.path.exists(folder_path):
-            print("Folder csv exists!")
             files_present += 1
         else:
             raise ValueError("Folder csv does not exist")
 
         if os.path.exists(specimen_path):
-            files_present += 1
             print("Specimen csv exists!")
+            files_present += 1
         else:
             raise ValueError("Specimen csv does not exist")
+    else:
+        raise ValueError(f"subdirectory for {date.today()} not present")
 
     return files_present
 
-
 # change filename path manually for now
 
-# def file_empty():
-"""will test if csv files contain rows or not"""
 
+# def file_empty(date_override=None, new_date=None):
+#     """will test if csv files contains any rows or not"""
+#
+#     if date_override is None:
+#         folder_date = date.today()
+#
+#     elif date_override is True:
+#         folder_date = new_date
+#
+#     path = (str("picturae_csv/") + str(folder_date))
+#
+#     folder_path = pd.read(path + '/picturae_folder(' + str(folder_date) + ').csv')
+#
+#     specimen_path = pd.read(path + '/specimen_folder(' + str(folder_date) + ').csv')
+#
+#     # creating empty boolean for folder csv
+#     folder_csv = pd.read_csv(folder_path)
+#
+#     folder_empty = folder_csv.empty
+#
+#     # creating empty boolean for specimen csv
+#
+#     specimen_csv = pd.read_csv(specimen_path)
+#
+#     specimen_empty = specimen_csv.empty
+#     # Raises for empty dataframe
+#     if folder_empty is True:
+#         raise ValueError("Folder csv contains no data!")
+#
+#     if specimen_empty is True:
+#         raise ValueError("Specimen csv contains no data!")
+#
+#     print("csv files are populated")
+
+
+def csv_read_folder(override=None, new_date=None):
+    """reads in folder_csv data for given date"""
+    folder_date = None
+    if override is None:
+        folder_date = date.today()
+    if override is True:
+        folder_date = new_date
+
+    folder_path = 'picturae_csv/' + str(folder_date) + '/picturae_folder(' + \
+                   str(folder_date) + ').csv'
+
+    folder_csv = pd.read_csv(folder_path)
+
+    return folder_csv
+
+
+def csv_read_specimen(override=None, new_date=None):
+    """reads in specimen_csv data fro given date"""
+    folder_date = None
+    if override is None:
+        folder_date = date.today()
+    if override is True:
+        folder_date = new_date
+    specimen_path = 'picturae_csv/' + str(folder_date) + '/picturae_specimen(' + \
+                     str(folder_date) + ').csv'
+
+    specimen_csv = pd.read_csv(specimen_path)
+
+    return specimen_csv
+
+
+### will merge both csv files into a master csv file
+# def csv_merge():
 
 def master_fun():
     """runs all functions"""
-    file_present(file_present(date_override=True, new_date="2023-6-28"))
+    # file_present()
+    file_present(date_override=True, new_date="2023-6-28")
+
+
+master_fun()
+
