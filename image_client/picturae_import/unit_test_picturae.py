41402fa039bb76d97cca470670a10dda0df0ef48
delarocamcas <mdelaroca@calacademy.org>
2023-06-30 15:45:30 -0700

    cleaned up pep8 errors, added comment strings to every test function, added more comment notes in main py file

--- image_client/picturae_import/unit_test_picturae.py
+++ image_client/picturae_import/unit_test_picturae.py
@@ -2,25 +2,29 @@
 import csv
 import unittest
 import random
-import os
 import shutil
 from picturae_import import *
 from faker import Faker
 from datetime import date, timedelta
 
-## need to find a way to prevent the fake folders using todays date in the setUP,
-## from overwriting the contents of real folders
+# need to find a way to prevent the fake folders using todays date in the setUP,
+# from overwriting the contents of real folders
+
+
 def test_date():
-    test_date = date.today() - timedelta(days=365 * 20)
-    return test_date
+    """test_date: creates an arbitrary date, 20 years in the past from today's date,
+       to create test files for, so as not to overwrite current work
+       ! if this code outlives 20 years of use I would be impressed"""
+    unit_date = date.today() - timedelta(days=365 * 20)
+    return str(unit_date)
 
 
 class WorkingDirectoryTests(unittest.TestCase):
-    """TestFilePresent: a series of unit tests on the file_present
-       function in picturae_import.py"""
+    """WorkingDirectoryTests: a series of unit tests to verify
+        correct working directory, subdirectories."""
 
     def test_working_directory(self):
-        """test if user in correct working folder"""
+        """test if user in correct working folder picturae import"""
         expected_relative = "picturae_import"
         current_dir = os.getcwd()
         _, last_directory = os.path.split(current_dir)
@@ -32,16 +36,20 @@ class WorkingDirectoryTests(unittest.TestCase):
         self.assertTrue(dir_pre)
 
     def test_missing_folder_raise_error(self):
-        """checks if incorrect sub_directory raises error"""
+        """checks if incorrect sub_directory raises error from file present"""
+        date_string = test_date()
         with self.assertRaises(ValueError) as cm:
-            file_present()
+            file_present(date_string)
         self.assertEqual(str(cm.exception), f"subdirectory for {date.today()} not present")
 
 
 class FilePathTests(unittest.TestCase):
-    """tests paths for file_present function in fixture scope"""
+    """ FilePathTests: tests paths for file_present
+       function using dummy paths. """
 
     def setUp(self):
+        """setUP: unittest setup function creates empty csvs,
+                  folders for given test path"""
 
         print("setup called!")
         # create test directories
@@ -60,33 +68,39 @@ class FilePathTests(unittest.TestCase):
         open(expected_specimen_path, 'a').close()
 
     def test_expected_path_date(self):
-        """makes temporary folders, and csvs with today's date, to test function,
-          when not overriden"""
+        """test_expected_path_date: makes temporary folders,
+           and csvs with test_date, to test function"""
+        date_string = test_date()
         try:
-            file_present()
+            file_present(date_string)
         except Exception as e:
             self.fail(f"Exception raised: {str(e)}")
 
     def test_raise_specimen(self):
+        """test_raise_specimen: tests whether correct value
+           error is raised for missing specimen csv"""
         date_string = test_date()
         # removing test path specimen
         os.remove('picturae_csv/' + str(date_string) + '/picturae_specimen(' +
                   str(date_string) + ').csv')
         with self.assertRaises(ValueError) as cm:
-            file_present()
+            file_present(date_string)
         self.assertEqual(str(cm.exception), "Specimen csv does not exist")
 
     def test_raise_folder(self):
+        """test_raise_folder: tests whether correct value error
+           is raised for missing folder csv"""
         date_string = test_date()
         # removing test path folder
         os.remove('picturae_csv/' + str(date_string) + '/picturae_folder(' +
                   str(date_string) + ').csv')
         with self.assertRaises(ValueError) as cm:
-            file_present()
+            file_present(date_string)
         self.assertEqual(str(cm.exception), "Folder csv does not exist")
 
     def tearDown(self):
-        """removes path for given created folder"""
+        """destroys paths for Setup function,
+           returning working directory to prior state"""
 
         print("teardown called!")
 
@@ -94,36 +108,40 @@ class FilePathTests(unittest.TestCase):
         # create test directories
 
         expected_folder_path = 'picturae_csv/' + str(date_string) + '/picturae_folder(' + \
-                                str(date_string) + ').csv'
+                               str(date_string) + ').csv'
         shutil.rmtree(os.path.dirname(expected_folder_path))
 
 
-
-### class for testing csv_import function
+# class for testing csv_import function
 # under construction
 class CsvReadMergeTests(unittest.TestCase):
+    """this class contains methods which test outputs of the
+       csv_read_folder function , and csv_merge functions from
+       picturae_import.py"""
 
-    # thinking of ways to simplify this setup function
+    # will think of ways to shorten this setup function
     def setUp(self):
-        """creates fake datasets with dummy columns to test import csv"""
+        """creates fake datasets with dummy columns,
+          that have a small subset of representive real column names,
+          so that test merges and uploads can be performed.
+          """
         print("setup called!")
-        # setting records and date list
+        # setting num records and test date
         fake = Faker()
         num_records = 50
         date_string = test_date()
-        # maybe create up a seperate function for setting up test directories
-            # setting string paths
+        # maybe create a separate function for setting up test directories
         path_type_list = ['folder', 'specimen']
         path_list = []
         for path_type in path_type_list:
-            path = 'picturae_csv/' + str(date_string) + '/picturae_'+ str(path_type) + '(' + \
+            path = 'picturae_csv/' + str(date_string) + '/picturae_' + str(path_type) + '(' + \
                     str(date_string) + ').csv'
 
-        path_list.append(path)
+            path_list.append(path)
 
-        os.makedirs(os.path.dirname(path), exist_ok=True)
+            os.makedirs(os.path.dirname(path), exist_ok=True)
 
-        open(path, 'a').close()
+            open(path, 'a').close()
         # writing csvs
         for path in path_list:
             with open(path, 'w', newline='') as csvfile:
@@ -141,7 +159,6 @@ class CsvReadMergeTests(unittest.TestCase):
                     # writing data to CSV
                     writer.writerow([specimen_bar, folder_barcode, jpg_path])
             print(f"Fake dataset {path} with {num_records} records created sucessfuly")
-
 
     def test_file_empty(self):
         """tests for every argument variation if dataset returns as empty"""
@@ -162,37 +179,49 @@ class CsvReadMergeTests(unittest.TestCase):
         self.assertEqual('specimen_barcode' in csv_read_folder('folder', date_string).columns, True)
         self.assertEqual('specimen_barcode' in csv_read_folder('specimen', date_string).columns, True)
 
-    ### these tests are for the csv merge function
+    # these tests are for the csv merge function
     def test_merge_num_columns(self):
-        csv_specimen = csv_read_folder('specimen')
-        csv_folder = csv_read_folder('folder')
-        # -2 as merge function drops duplicate columns automatically
+        """test merge with sample data set , to check if shared columns are removed,
+           and that the merge occurs with expected columns"""
+        date_string = test_date()
+        csv_specimen = csv_read_folder('specimen', import_date=date_string)
+        csv_folder = csv_read_folder('folder', import_date=date_string)
+        # -3 as merge function drops duplicate columns automatically
         self.assertEqual(len(csv_merge(csv_specimen, csv_folder).columns),
                          len(csv_specimen.columns) + len(csv_folder.columns) - 3)
 
-
     def test_index_length_matches(self):
-        csv_folder = csv_read_folder('folder')
-        csv_specimen = csv_read_folder('specimen')
+        """checks whether dataframe, length changes,
+           as folder and specimen csvs should
+           always be 100% matches on barcodes
+           """
+        date_string = test_date()
+        csv_folder = csv_read_folder('folder', import_date=date_string)
+        csv_specimen = csv_read_folder('specimen', import_date=date_string)
         # test merge index before and after
         self.assertEqual(len(csv_merge(csv_folder, csv_specimen)),
                          len(csv_folder))
 
     def test_unequalbarcode_raise(self):
-        csv_folder = csv_read_folder('folder')
-        csv_specimen = csv_read_folder('specimen')
+        """checks whether inserted errors in barcode raise
+           a Value error raise in the merge function"""
+        date_string = test_date()
+        csv_folder = csv_read_folder('folder', date_string)
+        csv_specimen = csv_read_folder('specimen', date_string)
         csv_specimen['specimen_barcode'] = csv_specimen['specimen_barcode'] + 1
         with self.assertRaises(ValueError) as cm:
             csv_merge(csv_folder, csv_specimen)
 
-        self.assertEqual(str(cm.exception), "Barcode Columns do not match!" )
+        self.assertEqual(str(cm.exception), "Barcode Columns do not match!")
 
     def test_output_isnot_empty(self):
-        csv_folder = csv_read_folder('folder')
-        csv_specimen = csv_read_folder('specimen')
+        """tests whether merge function
+           produces empty dataframe"""
+        date_string = test_date()
+        csv_folder = csv_read_folder('folder', import_date=date_string)
+        csv_specimen = csv_read_folder('specimen', import_date=date_string)
         # testing output
         self.assertFalse(csv_merge(csv_folder, csv_specimen).empty)
-
 
     def tearDown(self):
         """deletes destination directories of dummy datasets"""
@@ -209,4 +238,3 @@ class CsvReadMergeTests(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
