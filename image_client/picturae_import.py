f52754864cd5db906b2688aeafb4fd2579d4caf5
delarocamcas <mdelaroca@calacademy.org>
2023-07-12 17:40:10 -0700

    created 4  skeleton create functions, for locality, collectionobject, collectionevent, determiner, and added a function to create parameters for skeleton record

--- image_client/picturae_import.py
+++ image_client/picturae_import.py
@@ -1,19 +1,22 @@
 """this file will be used to parse the data from Picturae into an uploadable format to Specify"""
-import sys
-
 import picturae_config
+import sys
 import os
+import time_utils
+from uuid import uuid4
 from datetime import date
 from data_utils import *
 import pandas as pd
 from importer import Importer
 from db_utils import DbUtils
-from specify_db import SpecifyDb
-from botany_importer import BotanyImporter
 import logging
 
 
 class DataOnboard(Importer):
+    """DataOnboard: A class with methods designed to wrangle, verify, and upload a csv file
+       containing transcribed specimen sheet records into the casbotany database, along with attached,
+       images
+       """
     def __init__(self, date_string):
         super().__init__(picturae_config, "Botany")
         self.date_use = date_string
@@ -22,6 +25,20 @@ class DataOnboard(Importer):
         self.image_list = []
         self.record_full = pd.DataFrame
 
+        # initializing all data fields
+        self.barcode = None
+        self.verbatim_date = None
+        self.start_date = None
+        self.end_date = None
+        self.collector_number = None
+        self.locality = None
+        self.collecting_event_guid = None
+        self.locality_guid = None
+        self.geography_string = None
+        self.GeographyID = None
+        self.locality_id = None
+
+    @staticmethod
     def to_current_directory(self):
         """to_current_directory: changes current directory to .py file location
             args:
@@ -35,11 +52,10 @@ class DataOnboard(Importer):
 
         os.chdir(directory)
 
-
     def file_present(self):
         """file_present:
            checks if correct filepath in working directory,
-           checks if file is on current date, or input date
+           checks if file is on input date
            checks if file folder is present
            args:
                 override: takes boolean to specify whether to override today's date for custom date
@@ -89,11 +105,11 @@ class DataOnboard(Importer):
         fold_csv = self.csv_read_folder("folder")
         spec_csv = self.csv_read_folder("specimen")
 
-    # checking if columns to merge contain same data
+        # checking if columns to merge contain same data
         if (set(fold_csv['specimen_barcode']) == set(spec_csv['specimen_barcode'])) is True:
-        # removing duplicate columns
-        # (Warning! will want to double-check whether these columns are truly the
-        # same between datasets when more info received)
+            # removing duplicate columns
+            # (Warning! will want to double-check whether these columns are truly the
+            # same between datasets when more info received)
 
             common_columns = list(set(fold_csv.columns).intersection(set(spec_csv.columns)))
 
@@ -107,10 +123,6 @@ class DataOnboard(Importer):
 
         else:
             raise ValueError("Barcode Columns do not match!")
-
-
-
-
 
     def csv_colnames(self):
         """csv_colnames: function to be used to rename columns to specify standards"""
@@ -128,7 +140,10 @@ class DataOnboard(Importer):
 
         # some of these are just placeholders for now
 
-        col_dict = {'specimen_barcode': 'CatalogNumber',
+        col_dict = {'Country': 'country',
+                    'State': 'state',
+                    'County': 'county',
+                    'specimen_barcode': 'CatalogNumber',
                     'path_jpg': 'image_path',
                     'collector_number': 'collector_number',
                     'collector_first_name 1': 'collector_first_name1',
@@ -142,33 +157,32 @@ class DataOnboard(Importer):
                     'Qualifier': 'Qualifier',
                     'Hybrid': 'Hybrid',
                     'Taxon ID': 'RankID',
-                    'Author': 'Author'}
+                    'Author': 'Author',
+                    'Locality': 'locality',
+                    'Verbatim Date': 'verbatim_date',
+                    'Start Date': 'start_date',
+                    'End Date': 'end_date'
+
+                     }
 
         self.record_full = self.record_full.rename(columns=col_dict)
 
-
-    # def mapping_AuthorID(df: pd.DataFrame):
-
-    # def mapping_LocalityID(df: pd.Dataframe):
-
-    # def mapping_TaxonID(df: pd.Dataframe):
-
-
     # under this point column transformations will be done through a series of functions
     # will reuse/modify some wrangling functions from data standardization
-
 
     # def col_clean():
     #    """will reformat and clean dataframe until ready for upload.
     #       **Still need format end-goal
     #       """
 
-
     # after file is wrangled into clean importable form,
     # and QC protocols to follow before import
     # QC measures needed here ? before proceeding.
 
+    # it is faster to make booleans now, and filter later in the process function
+
     def barcode_has_record(self):
+        """check if barcode / catalog number already in collectionobject table"""
         self.record_full['CatalogNumber'] = self.record_full['CatalogNumber'].apply(remove_non_numerics)
         self.record_full['CatalogNumber'] = self.record_full['CatalogNumber'].astype(str)
         self.record_full['barcode_present'] = None
@@ -183,9 +197,8 @@ class DataOnboard(Importer):
             else:
                 row['barcode_present'] = True
 
-
     def image_has_record(self):
-
+        """checks if image name/barcode already on attachments table"""
         self.record_full['image_present'] = None
         for index, row in self.record_full.iterrows():
             file_name = os.path.basename(row['image_path'])
@@ -197,13 +210,6 @@ class DataOnboard(Importer):
                 row['image_present'] = True
             else:
                 row['image_present'] = False
-
-
-# def check_attachment(df: pd.DataFrame):
-
-# under construction
-    # although this could be done on a row be row basis,
-    # it is faster to make booleans now, and filter later in the process function
 
     def check_barcode_match(self):
         """checks if filepath barcode matches catalog number barcode"""
@@ -262,12 +268,140 @@ class DataOnboard(Importer):
                 else:
                     print("Invalid input. Please enter 'y' or 'n'.")
 
-    def create_csv_skeleton(self):
-        """used to assign columns to database tables using sql"""
-        self.record_full = self.record_full[self.record_full['CatalogNumber'].isin(self.barcode_list)]
-        for index, row in self.record_full.iterrows():
-            row
 
+    def populate_fields(self, row):
+        self.barcode = row['CatalogNumber'].zfill(9)
+        self.verbatim_date = row['verbatim_date']
+        self.start_date = row['start_date']
+        self.end_date = row['end_date']
+        self.collector_number = row['collector_number']
+        self.locality = row['locality']
+        self.collecting_event_guid = uuid4()
+        self.locality_guid = uuid4()
+        self.geography_string = str(row['county']) + ", " + str(row['state']) + ", " + str(row['country'])
+
+        # Locality create
+        sql = f"SELECT GeographyID FROM geography where `FullName` = {self.geography_string};"
+        self.GeographyID = self.specify_db_connection.get_one_record(sql)
+
+        sql = f"select LocalityID from casbotany.locality where Locality='{row['locality']}'"
+        self.locality_id = self.specify_db_connection.get_one_record(sql)
+
+    def create_locality_record(self, row):
+        """used to assign columns to database tables using sql"""
+        self.logger.info(f"Creating skeleton for csv barcode {row['CatalogNumber']}")
+
+        # assigning row ids
+        if self.locality_id is None:
+            # locality_id = some number
+            cursor = self.specify_db_connection.get_cursor()
+            sql = (f'''INSERT INTO casbotany.locality (
+                       LocalityID,
+                       TimestampCreated,
+                       TimestampModified,
+                       Version,
+                       GUID,
+                       LocalityName,
+                       DisciplineID,
+                       GeographyID
+                       )
+                       VALUES (
+                        
+                       '{time_utils.get_pst_time_now_string()}',
+                       '{time_utils.get_pst_time_now_string()}',
+                        1,
+                       '{self.locality_guid}',
+                       '{row['locality']}',
+                        3,
+                       '{self.GeographyID}' 
+                       )''')
+            self.logger.debug(sql)
+            cursor.execute(sql)
+            self.specify_db_connection.commit()
+
+
+def create_collectingevent(self, row):
+        # will add accession number
+        cursor = self.specify_db_connection.get_cursor()
+        sql =(f'''INSERT INTO casbotany.collectingevent (
+                  TimestampCreated,
+                  TimestampModified,
+                  Version,
+                  GUID,
+                  DisciplineID,
+                  StationFieldNumber,
+                  VerbatimDate,
+                  StartDate,
+                  EndDate,
+                  LocalityID
+                  )
+                  VALUES (
+                 '{time_utils.get_pst_time_now_string()}',
+                 '{time_utils.get_pst_time_now_string()}',
+                  0,
+                 '{self.collecting_event_guid}',
+                  3,
+                 '{self.collector_number}',
+                 '{self.verbatim_date}',
+                 '{self.start_date}',
+                 '{self.end_date}',
+                 '{self.locality_id}'
+                  )''')
+        self.logger.debug(sql)
+        cursor.execute(sql)
+        self.specify_db_connection.commit()
+
+        # collection object create
+
+
+def create_collection_object(self, row):
+        sql = f"select CollectingEventID from casbotany.collectingevent where guid='{collecting_event_guid}'"
+        collecting_event_id = self.specify_db_connection.get_one_record(sql)
+
+        cursor = self.specify_db_connection.get_cursor()
+
+        sql = (f'''INSERT INTO casbotany.collectionobject (
+                  TimestampCreated,
+                  TimestampModified,
+                  CollectingEventID,
+                  Version,
+                  CollectionMemberID,
+                  CatalogNumber,
+                  CatalogedDatePrecision,
+                  GUID,
+                  CollectionID,
+                  Date1Precision,
+                  InventoryDatePrecision    
+                  )
+        
+                  )
+                  VALUES (
+                  '{time_utils.get_pst_time_now_string()}',
+                  '{time_utils.get_pst_time_now_string()}'),
+                  '{collecting_event_id}',
+                  0,
+                  4,
+                  '{self.barcode}',
+                  1,
+                  '{uuid4()}',
+                  4,
+                  1,
+                  1  )
+                   ''')
+
+        self.logger.debug(sql)
+        cursor.execute(sql)
+        self.specify_db_connection.commit()
+
+
+def determiner_create(self, row):
+    pass
+
+#if determiner is None:
+# cursor = self.specify_db_connection.get_cursor()
+
+
+    # def csv_barcode_process:
 
     # def run_all_methods(self):
     #     # setting directory
@@ -286,4 +420,3 @@ class DataOnboard(Importer):
     #     full_frame = self.check_barcode_match()
     #     # creating skeleton
     #     # finished
-
