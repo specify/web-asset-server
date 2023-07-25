9436e7e2b66eb939f6e46a637ac0ba72949f69dc
delarocamcas <mdelaroca@calacademy.org>
2023-07-25 09:46:03 -0700

    completed adding determination, collector sql inserts, added rudementry taxon checker (to be expanded) , added protections against NA values, added list and sql util functions

--- picturae_purge_script.sql
+++ picturae_purge_script.sql
@@ -1,9 +1,8 @@
 DELETE FROM casbotany.collectionobjectattachment WHERE TimestampCreated > (NOW() - INTERVAL 10 DAY);
 DELETE FROM casbotany.attachment WHERE TimestampCreated > (NOW() - INTERVAL 10 DAY);
+DELETE FROM casbotany.determination WHERE TimestampCreated > (NOW() - INTERVAL 10 DAY);
 DELETE FROM casbotany.collectionobject WHERE TimestampCreated > (NOW() - INTERVAL 10 DAY);
+DELETE FROM casbotany.collector WHERE  TimestampCreated > (NOW() - INTERVAL 10 DAY);
 DELETE FROM casbotany.collectingevent WHERE TimestampCreated > (NOW() - INTERVAL 10 DAY);
 DELETE FROM casbotany.locality WHERE TimestampCreated > (NOW() - INTERVAL 10 DAY);
 DELETE FROM casbotany.agent WHERE TimestampCreated > (NOW() - INTERVAL 10 DAY);
-DELETE FROM casbotany.collectionobjectattachment WHERE TimestampCreated > (NOW() - INTERVAL 10 DAY);
-DELETE FROM casbotany.collectionobject WHERE TimestampCreated > (NOW() - INTERVAL 10 DAY);
-
