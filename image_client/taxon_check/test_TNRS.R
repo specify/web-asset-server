55e668235d3401768adde5444934b941b3824a65
delarocamcas <mdelaroca@calacademy.org>
2023-08-09 16:22:31 -0700

    added iterative taxon adding system, made adjustments for subtaxa authors, changed protocols ofor NA values, made other minor fixes

--- image_client/taxon_check/test_TNRS.R
+++ image_client/taxon_check/test_TNRS.R
@@ -19,7 +19,7 @@ process_taxon_resolve <- function(tax_frame){
   
   taxon_frame = tax_frame
   
-  
+  ncol_t = nrow(taxon_frame)
   #test_taxon = read_csv("test_csv.csv")
 
   
@@ -55,27 +55,24 @@ process_taxon_resolve <- function(tax_frame){
   
   results <- as.data.frame(results_raw)
   
-  print(results)
-  
-  
   # to better compare the output fields
   results.t <- as.data.frame( t( results[,1:ncol(results)] ) )
-  results.t[,3,drop =FALSE]
+  results.t[,ncol_t,drop =FALSE]
   
   # Display just the main results fields
   results$match.score <- format(round(as.numeric(results $Overall_score),2), nsmall=2)
-  
-  print(results)
+
   
   results[ 1:10, c('Name_submitted', 'match.score', 'Name_matched', 'Taxonomic_status', 
-                   'Accepted_name', 'Unmatched_terms')]
+                   'Accepted_name', 'Unmatched_terms', 'Accepted_name_author')]
   
   results = results %>% rename('fullname'='Name_submitted', 
                                'name_matched' = 'Name_matched',
                                'taxonomic_status' = 'Taxonomic_status',
                                'accepted_name'= 'Accepted_name',
-                               'unmatched_terms'='Unmatched_terms',
-                               'overall_score' = 'Overall_score')
+                               'unmatched_terms'= 'Unmatched_terms',
+                               'overall_score' = 'Overall_score',
+                               'accepted_author' = 'Accepted_name_author')
   
 
   hand_check_match = results %>% filter(overall_score < .985) %>% 
@@ -99,7 +96,7 @@ process_taxon_resolve <- function(tax_frame){
   
   
   results = results %>% filter(overall_score > .99) %>% 
-            select('fullname', 'name_matched', 'overall_score')
+            select('fullname', 'name_matched', 'accepted_author','overall_score')
   
   return(results)
 }
@@ -109,16 +106,19 @@ resolved_taxa = process_taxon_resolve(tax_frame = r_dataframe_taxon)
 
 sink()
 
-#test_taxon = list(barcodes = c(1234, 1235, 1236, 1237),
-                #  fullname = c('cf. Arctostaphylos glandulosa', 
-                   #            'Aesculus cf. californica', 
-                   #           'Impatiens banen', 
-                       #     'Quercus x kinselae'))
+# test_taxon = list(barcodes = c(1234),
+               # fullname = c('Abies sibirica subsp. semenovii'))
 
 # test_frame = do.call(data.frame, test_taxon)
 
 # returnti = process_taxon_resolve(tax_frame = test_frame)
 
+
+# test_taxon = list(barcodes= c(999999984), fullname = c('Abies sibirica'))
+
+# test_frame = do.call(data.frame, test_taxon)
+
+# returnti = process_taxon_resolve(tax_frame = test_frame)
 
 
 
