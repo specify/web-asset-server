5f4668bcc2f9fafb7ffe023211c844b4dfba0e68
delarocamcas <mdelaroca@calacademy.org>
2023-08-31 12:02:00 -0700

    added some changes to picturae_config.template

--- image_client/picturae_config.template.py
+++ image_client/picturae_config.template.py
@@ -4,3 +4,14 @@ SPECIFY_DATABASE_PORT = 3306
 SPECIFY_DATABASE = 'redacted'
 USER = 'redacted'
 PASSWORD = 'redacted'
+
+
+sla = os.path.sep
+# config paths files
+date_str = "2023-06-28"
+
+PREFIX = f'picturae_img{sla}'
+
+SCAN_FOLDERS = [f'PIC_{date_str}']
+
+DIR = "/Users/mdelaroca/Documents/sandbox_db/specify-sandbox/web-asset-server/image_client/"
